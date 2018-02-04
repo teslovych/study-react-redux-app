8ea978042f714a6d53daed37170d064b1c3d7c69
Ivan Teslovych <i.teslovich@mobilunity.com>
2018-02-04 17:24:11 +0200

    Finished with containers

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import BooksList from '../containers/books-list.component';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+        <div>
+          <BooksList />
+        </div>
     );
   }
 }
